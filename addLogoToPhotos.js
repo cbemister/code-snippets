1af77e8016c256093e03fa6c64b6af180e15f449
Chris <cbemister@autocan.ca>
2019-01-10 13:53:48 -0700

    Removed unecessary parameter - cleaned up code

--- addLogoToPhotos.js
+++ addLogoToPhotos.js
@@ -23,24 +23,24 @@ dl#activeStaff-item img.chairman-winner {
 (function  () {
 
 	// Add Chairman's Club Winners
-	var chairmansClubWinners = ['Dale Roach', 'Sean Miles', 'winner3'] 
-	// First boolean - Chairman's Club winner is the default selection  
-	// Second boolean - Page organized by departments  
+	var chairmansClubWinners = ['winner1', 'winner2', 'winner3'] 
+	// First boolean - is page organized by departments    
+	// Second boolean -  is website mobile
 	addChairmanLogo(chairmansClubWinners, true, false); 	
 
-	function addChairmanLogo(a,e,t){var i=new Date("December 31, 2019 19:59:59"),n=new Date;if(i.getTime()/1e3-n.getTime()/1e3>=0){var s=e,c=a[0],o=a[1],r=a[2];if(s)addLogoToSelected($("dl#activeStaff-item dt.fn"));$(t?"div#staffList .staff dt.fn":"ul#staffList dt.fn").each(function(a,e){switch(e.innerText){case c:case o:case r:addLogoToList(this,e)}}),$("ul#staffList").after("<div>The Chairman's club ...</div>")}}function addLogoToSelected(a){$(a).parent().css("position","relative"),$(a).parent().append('<img src="//pictures.dealer.com/a/autocanadatc/0520/5e3d11c9f55384173fb31bb3ea862508x.jpg" alt="Chairman\'s Club Winner" class="chairman-winner" />')}function addLogoToList(a,e){$(a).parent().css("position","relative"),$(e).siblings(".photo").after('<img src="//pictures.dealer.com/a/autocanadatc/0520/5e3d11c9f55384173fb31bb3ea862508x.jpg" alt="Chairman\'s Club Winner" class="chairman-winner"/>')}
+	function addChairmanLogo(a,t,e){var i=new Date("December 31, 2019 19:59:59"),s=new Date;if(i.getTime()/1e3-s.getTime()/1e3>=0){var n,f=a[0],c=a[1],r=a[2];n=e&&t?"div#staffList .staff-member dt.fn":t?"div#staffList .staff dt.fn":"ul#staffList dt.fn",$(n).each(function(a,t){switch($(this).find("span").attr("name")){case f:case c:case r:addLogoToList(this,t)}}),$("ul#staffList").after("<div>The Chairman's club ...</div>")}}function addLogoToList(a,t){$(a).parent().css("position","relative"),$(t).siblings(".photo").after('<img src="//pictures.dealer.com/a/autocanadatc/0520/5e3d11c9f55384173fb31bb3ea862508x.jpg" alt="Chairman\'s Club Winner" class="chairman-winner"  />')}
 
 }())
 
 
 (function(){
 	// Add Chairman's Club Winners
-	var chairmansClubWinners = ['Dale Roach', 'Sean Miles', 'winner3'] 
-	// First boolean - Chairman's Club winner is the default selection  
-	// Second boolean - Page organized by departments  
+	var chairmansClubWinners = ['winner1', 'winner2', 'winner3'] 
+	// First boolean - is page organized by departments    
+	// Second boolean -  is website mobile
 	addChairmanLogo(chairmansClubWinners, true, false); 	
 	
-	function addChairmanLogo(winners, featured, departmentLayout) {
+	function addChairmanLogo(winnersArray, departmentLayout, isMobile) {
 
 		//DATE DECLARATION
 		var expiryDate = new Date("December 31, 2019 19:59:59"); //Month Days, Year HH:MM:SS
@@ -49,52 +49,48 @@ dl#activeStaff-item img.chairman-winner {
 
 		//SHOW IF WITHIN RANGE	
 		if (diffDate >= 0) {
-			// Set to true if a winner is the selected photo	
-			// Set to false on mobile - No selected photo
-			var selectedWinner = featured;
-
+			
 			// Set winner names from list
-			var winner1 = winners[0];
-			var winner2 = winners[1];
-			var winner3 = winners[2];
+			var winner1 = winnersArray[0];
+			var winner2 = winnersArray[1];
+			var winner3 = winnersArray[2];
 
-			// Add logo to selected staff if applicable
-			if (selectedWinner) {
-				var selectedStaffNode = $('dl#activeStaff-item dt.fn');
-				addLogoToSelected(selectedStaffNode);
+			// Set staff element selector
+			var staffSelector;
+			
+			if (isMobile && departmentLayout ) {
+				staffSelector =  'div#staffList .staff-member dt.fn'
+			} else {
+				staffSelector = departmentLayout ? 'div#staffList .staff dt.fn' : 'ul#staffList dt.fn';
 			}
-
+						
 			// Loop through main staff listings
 				
-				var staffSelector = departmentLayout ? 'div#staffList .staff dt.fn' : 'ul#staffList dt.fn';
-				
-				$(staffSelector).each(function (staffIndex, staffValue) {
 
-					switch (staffValue.innerText) {
-						case winner1:
-							addLogoToList(this, staffValue);
-							break;
-						case winner2:
-							addLogoToList(this, staffValue);
-							break;
-						case winner3:
-							addLogoToList(this, staffValue);
-							break;
-						default:
-							break;
-					}
+			$(staffSelector).each(function (staffIndex, staffValue) {
 
-				});
+				var staffName = $(this).find('span').attr('name');
+
+				switch (staffName) {
+					case winner1:
+						addLogoToList(this, staffValue);
+						break;
+					case winner2:
+						addLogoToList(this, staffValue);
+						break;
+					case winner3:
+						addLogoToList(this, staffValue);
+						break;
+					default:
+						break;
+				}
+
+			});
 			
 		$("ul#staffList").after("<div>The Chairman's club ...</div>");
 
 		}
 
-	}
-
-	function addLogoToSelected(node) {
-		$(node).parent().css('position', 'relative');
-		$(node).parent().append('<img src="//pictures.dealer.com/a/autocanadatc/0520/5e3d11c9f55384173fb31bb3ea862508x.jpg" alt="Chairman\'s Club Winner" class="chairman-winner" />');
 	}
 
 	function addLogoToList(item, staff) {
